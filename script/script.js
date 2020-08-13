82ec89f7026f91a5bc81c50483b863e535a8a38a
Alexey Arhipiv <alex3171@list.ru>
2020-08-13 16:52:52 +0500

    feat: Home work advaced

--- script/script.js
+++ script/script.js
@@ -1,28 +1,49 @@
 "use strict";
 
-/* function random() {
-  return Math.floor(Math.random() * 100) + 1;
-} */
+let count = 1;
 
 function getNumber() {
   let randomNumber = 47;
 
   alert("Угадайте число от 1 до 100");
-  let answer = prompt("Угадайте число от 1 до 100");
 
-  if (answer === null) {
-    alert("Конец игры");
-  } else if (answer.trim() === "" || answer === undefined || isNaN(answer)) {
-    alert("Введи число!");
-    getNumber();
-  } else if (+answer === randomNumber) {
-    alert("Поздравляю, Вы угадали!!!");
-  } else if (+answer > randomNumber) {
-    alert("Загаданное число меньше");
-    getNumber();
-  } else if (+answer < randomNumber) {
-    alert("Загаданное число больше");
-    getNumber();
+  let answer = prompt("Угадайте число от 1 до 100");
+  //условие на 10 попыток
+  if (count < 10) {
+    //проверка на нажатие кнопки отмена
+    if (answer === null) {
+      alert("Конец игры");
+      //проверка на число
+    } else if (answer.trim() === "" || answer === undefined || isNaN(answer)) {
+      alert("Введи число!");
+      getNumber();
+    } else if (+answer === randomNumber) {
+      let gameNew = confirm("Поздравляю, Вы угадали!!! Хотели бы сыграть еще?");
+      if (gameNew) {
+        //обнуление счетчика и запуск игры снова
+        count = 1;
+        getNumber();
+      } else {
+        alert("Конец игры");
+      }
+    } else if (+answer > randomNumber) {
+      alert("Загаданное число меньше, осталось попыток " + (10 - count));
+      //счетчик попыток
+      count++;
+      getNumber();
+    } else if (+answer < randomNumber) {
+      alert("Загаданное число больше, осталось попыток " + (10 - count));
+      count++;
+      getNumber();
+    }
+  } else {
+    let user = confirm("Попытки закончились, хотите сыграть еще?");
+    if (user) {
+      count = 1;
+      getNumber();
+    } else {
+      alert("Конец игры");
+    }
   }
 }
 
