f31975df2783ba7d7e82dc5b64451c29d6c6f690
Alexey Arhipiv <alex3171@list.ru>
2020-08-14 12:08:52 +0500

    fix: Home work

--- script/script.js
+++ script/script.js
@@ -1,50 +1,56 @@
 "use strict";
-
-let count = 1;
-
-function getNumber() {
-  let randomNumber = 47;
-
+let closure = () => {
   alert("Угадайте число от 1 до 100");
+  let count = 1;
+  let randomNumber = Math.floor(Math.random() * 100) + 1;
 
-  let answer = prompt("Угадайте число от 1 до 100");
-  //условие на 10 попыток
-  if (count < 10) {
-    //проверка на нажатие кнопки отмена
-    if (answer === null) {
-      alert("Конец игры");
-      //проверка на число
-    } else if (answer.trim() === "" || answer === undefined || isNaN(answer)) {
-      alert("Введи число!");
-      getNumber();
-    } else if (+answer === randomNumber) {
-      let gameNew = confirm("Поздравляю, Вы угадали!!! Хотели бы сыграть еще?");
-      if (gameNew) {
-        //обнуление счетчика и запуск игры снова
+  function getNumber() {
+    let answer = prompt("Угадайте число от 1 до 100");
+    //условие на 10 попыток
+    if (count < 10) {
+      //проверка на нажатие кнопки отмена
+      if (answer === null) {
+        alert("Конец игры");
+        //проверка на число
+      } else if (
+        answer.trim() === "" ||
+        answer === undefined ||
+        isNaN(answer)
+      ) {
+        alert("Введи число!");
+        getNumber();
+      } else if (+answer === randomNumber) {
+        let gameNew = confirm(
+          "Поздравляю, Вы угадали!!! Хотели бы сыграть еще?"
+        );
+        if (gameNew) {
+          //обнуление счетчика и запуск игры снова
+          count = 1;
+          getNumber();
+        } else {
+          alert("Конец игры");
+        }
+      } else if (+answer > randomNumber) {
+        alert("Загаданное число меньше, осталось попыток " + (10 - count));
+        //счетчик попыток
+        count++;
+        getNumber();
+      } else if (+answer < randomNumber) {
+        alert("Загаданное число больше, осталось попыток " + (10 - count));
+        count++;
+        getNumber();
+      }
+    } else {
+      let user = confirm("Попытки закончились, хотите сыграть еще?");
+      if (user) {
         count = 1;
         getNumber();
       } else {
         alert("Конец игры");
       }
-    } else if (+answer > randomNumber) {
-      alert("Загаданное число меньше, осталось попыток " + (10 - count));
-      //счетчик попыток
-      count++;
-      getNumber();
-    } else if (+answer < randomNumber) {
-      alert("Загаданное число больше, осталось попыток " + (10 - count));
-      count++;
-      getNumber();
-    }
-  } else {
-    let user = confirm("Попытки закончились, хотите сыграть еще?");
-    if (user) {
-      count = 1;
-      getNumber();
-    } else {
-      alert("Конец игры");
     }
   }
-}
+  getNumber();
+};
 
-getNumber();
+closure();
