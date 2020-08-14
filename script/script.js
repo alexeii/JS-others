8c152d3ee15c25cc5db24f8432bd60a28bb419f0
Alexey Arhipiv <alex3171@list.ru>
2020-08-14 12:12:49 +0500

    fix:Home work

--- script/script.js
+++ script/script.js
@@ -1,29 +1,29 @@
 "use strict";
 
-/* function random() {
-  return Math.floor(Math.random() * 100) + 1;
-} */
+function closure() {
+  let randomNumber = Math.floor(Math.random() * 100) + 1;
 
-function getNumber() {
-  let randomNumber = 47;
+  function getNumber() {
+    alert("Угадайте число от 1 до 100");
+    let answer = prompt("Угадайте число от 1 до 100");
 
-  alert("Угадайте число от 1 до 100");
-  let answer = prompt("Угадайте число от 1 до 100");
-
-  if (answer === null) {
-    alert("Конец игры");
-  } else if (answer.trim() === "" || answer === undefined || isNaN(answer)) {
-    alert("Введи число!");
-    getNumber();
-  } else if (+answer === randomNumber) {
-    alert("Поздравляю, Вы угадали!!!");
-  } else if (+answer > randomNumber) {
-    alert("Загаданное число меньше");
-    getNumber();
-  } else if (+answer < randomNumber) {
-    alert("Загаданное число больше");
-    getNumber();
+    if (answer === null) {
+      alert("Конец игры");
+    } else if (answer.trim() === "" || answer === undefined || isNaN(answer)) {
+      alert("Введи число!");
+      getNumber();
+    } else if (+answer === randomNumber) {
+      alert("Поздравляю, Вы угадали!!!");
+    } else if (+answer > randomNumber) {
+      alert("Загаданное число меньше");
+      getNumber();
+    } else if (+answer < randomNumber) {
+      alert("Загаданное число больше");
+      getNumber();
+    }
   }
+
+  getNumber();
 }
 
-getNumber();
+closure();
